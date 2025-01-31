5f9d04491da60f0a8570a19b76067547b86b3287
chojuninengu <chojuninengu@gmail.com>
2025-01-31 16:28:31 +0100

    Updated

--- start.sh
+++ start.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-
-IMAGE_NAME="rust-calculator"
-CONTAINER_NAME="rust-calculator-container"
-
-echo "Building the Docker image..."
-docker build -t $IMAGE_NAME .
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build the Docker image."
-    exit 1
-fi
-
-echo "Docker image built successfully."
-
-
-IMAGE_NAME="rust-calculator"
-CONTAINER_NAME="rust-calculator-container"
+IMAGE_NAME="rustacean"
+CONTAINER_NAME="rust-by-mr9-container"
 
 echo "Building the Docker image..."
 docker build -t $IMAGE_NAME .
@@ -28,16 +13,9 @@ fi
 
 echo "Docker image built successfully."
 
+echo "Removing any existing container with the name '$CONTAINER_NAME'..."
+docker rm -f $CONTAINER_NAME || true
 
-echo "Starting the Docker container..."
-docker run --name $CONTAINER_NAME -it $IMAGE_NAME /bin/bash
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to start the Docker container."ls
-    exit 1
-fi
-
-echo "You are now inside the Docker container. Type 'exit' to leave the container."
 echo "Starting the Docker container..."
 docker run --name $CONTAINER_NAME -it $IMAGE_NAME /bin/bash
 
